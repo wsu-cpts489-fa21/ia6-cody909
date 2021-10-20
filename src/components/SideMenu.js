8685b8970afee5b880f0522f7b5b3d34b596c0d8
Cody Mercadante <cody.mercadante@wsu.edu>
2021-10-19 19:24:18 -0700

    Implemented side menu fucntionality

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,23 +1,96 @@
-import React from 'react';
+import React from "react";
+import { useEffect, useRef } from "react";
 
-class SideMenu extends React.Component {
-    
-    render() {
-       return (
-        <ul id="sideMenu" role="menu" className="sidemenu"
-          arial-labelledby="menuBtn">
-            <li role="menuitem" tabIndex="-1">
-            Settings
-            </li>
-            <li role="menuitem" tabIndex="-1">
-            About
-            </li>
-            <li role="menuitem" tabIndex="-1">
-            Log Out
-            </li>
-        </ul>  
-    ); 
-  }
+function useOutsideAlerter(ref, toggleMenuOpen) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        toggleMenuOpen();
+      }
+    }
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [ref]);
+}
+
+function SideMenu(props) {
+  useEffect(() => {
+    function handleKeyDown(event) {
+      console.log("Key: " + event.key);
+      switch (event.key) {
+        case "ArrowUp":
+          if (settingsRef.current === document.activeElement) {
+            logoutRef.current.focus();
+          } else if (logoutRef.current === document.activeElement) {
+            aboutRef.current.focus();
+          } else if (aboutRef.current === document.activeElement) {
+            settingsRef.current.focus();
+          } else if (document.activeElement.id === "menuBtn") {
+            logoutRef.current.focus();
+          }
+          break;
+        case "ArrowDown":
+          if (settingsRef.current === document.activeElement) {
+            aboutRef.current.focus();
+          } else if (logoutRef.current === document.activeElement) {
+            settingsRef.current.focus();
+          } else if (aboutRef.current === document.activeElement) {
+            logoutRef.current.focus();
+          } else if (document.activeElement.id === "menuBtn") {
+            settingsRef.current.focus();
+          }
+          break;
+        case "s" || "S":
+          settingsRef.current.focus();
+          break;
+        case "l" || "L":
+          logoutRef.current.focus();
+          break;
+        case "a" || "A":
+          aboutRef.current.focus();
+          break;
+        case "Escape":
+          props.toggleMenuOpen();
+        default:
+          break;
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  const menuRef = useRef("null");
+  const settingsRef = useRef("null");
+  const aboutRef = useRef("null");
+  const logoutRef = useRef("null");
+
+  useOutsideAlerter(menuRef, props.toggleMenuOpen);
+
+  return (
+    <ul
+      ref={menuRef}
+      id="sideMenu"
+      role="menu"
+      className="sidemenu"
+      arial-labelledby="menuBtn"
+    >
+      <li ref={settingsRef} role="menuitem" tabIndex="-1">
+        Settings
+      </li>
+      <li ref={aboutRef} role="menuitem" tabIndex="-1">
+        About
+      </li>
+      <li ref={logoutRef} role="menuitem" tabIndex="-1">
+        Log Out
+      </li>
+    </ul>
+  );
 }
 
 export default SideMenu;
